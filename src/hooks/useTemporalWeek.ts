f77f23a809389d6e6eb24c0f9e20db675cff27c3
xarlizard <38315638+xarlizard@users.noreply.github.com>
2025-07-21 15:34:10 +0200

    added cusotm hook

---
+++ src/hooks/useTemporalWeek.ts
@@ -0,0 +1,13 @@
+import { useMemo } from 'react';
+import { Temporal } from '@js-temporal/polyfill';
+
+/**
+ * Returns all dates in the week of a given Temporal.PlainDate.
+ */
+export function useTemporalWeek(date: Temporal.PlainDate) {
+  return useMemo(() => {
+    const weekDay = date.dayOfWeek;
+    const start = date.subtract({ days: weekDay - 1 });
+    return Array.from({ length: 7 }, (_, i) => start.add({ days: i }));
+  }, [date]);
+}
