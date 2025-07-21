10fe91e42904da72785e73ca80567226633a6722
xarlizard <38315638+xarlizard@users.noreply.github.com>
2025-07-21 15:34:48 +0200

    uploaded test

---
+++ src/__tests__/useTemporalWeek.test.ts
@@ -0,0 +1,11 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useTemporalWeek } from '../hooks/useTemporalWeek';
+import { Temporal } from '@js-temporal/polyfill';
+
+describe('useTemporalWeek', () => {
+  it('returns all dates in the week', () => {
+    const date = Temporal.PlainDate.from('2025-07-21');
+    const { result } = renderHook(() => useTemporalWeek(date));
+    expect(result.current.length).toBe(7);
+  });
+});
